bf7dd598398103cec26fa2f03643e283772c34b2
banner4422 <christian@chrkn.dev>
2024-02-16 15:29:01 +0100

    feat(tags): add initial TagCommands

--- src/dotBento.Infrastructure/Commands/TagCommands.cs
+++ src/dotBento.Infrastructure/Commands/TagCommands.cs
@@ -1,6 +1,14 @@
+using CSharpFunctionalExtensions;
+using dotBento.Infrastructure.Services;
+
 namespace dotBento.Infrastructure.Commands;
 
-public class TagCommands
+public class TagCommands(TagService tagService)
 {
-    
+    public async Task<Result> CreateTagAsync(long userId, long guildId, string name, string content)
+    {
+        // TODO: Validate content before making the tag, like checking for mentions, etc.
+        var tag = await tagService.CreateTagAsync(userId, guildId, name, content);
+        return Result.Success(tag);
+    }
 }
