c0e83bcf6280912d58aa4cd7adc1b9f7231860f2
banner4422 <banner4422@github.com>
2024-04-16 22:26:34 +0200

    feat(Domain.Constants): Add lists of existing commands and aliases
    We should find a better solution at some point as this will become annoying to maintain

--- src/dotBento.Domain/Constants.cs
+++ src/dotBento.Domain/Constants.cs
@@ -7,4 +7,37 @@ public static class Constants
     public const ulong BotDevelopmentId = 790353119795871744;
 
     public const string StartPrefix = "_";
+
+    public static readonly IReadOnlyCollection<string> CommandNames = new List<string>
+    {
+        "About",
+        "Avatar",
+        "Banner",
+        "Bento",
+        "Choose",
+        "Game",
+        "LastFm",
+        "Ping",
+        "Server",
+        "Urban",
+        "User",
+        "Weather",
+        "deleteWeather",
+        "saveWeather",
+        "EightBall",
+        "Member",
+        "Roll",
+        "Rps",
+        "ServerInfo",
+    };
+    
+    public static readonly IReadOnlyCollection<string> AliasNames = new List<string>
+    {
+        "Pick",
+        "Av",
+        "Pfp",
+        "FM",
+        "guildMember",
+        "guildInfo",
+    };
 }
